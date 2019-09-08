99003865a02728ac5143bd52afae538b096cb2cd
JOAN <joan.corea.aguilar@est.una.ac.cr>
2019-09-08 12:51:30 -0600

    vector o entero en la lista

--- Proyecto_Datos/Lista.h
+++ Proyecto_Datos/Lista.h
@@ -7,15 +7,21 @@
 using namespace std;
 
 struct nodo_simple {
+	//int dato[4];
 	int dato;
 	struct nodo_simple* next;
 };
 
 //El metodo se tiene que cambiar, ya que ahora se va guardar en un vector todos los datos
 
-//Metodo que inserta un elemento a una pila
+//Metodo que inserta un elemento a una pila (ya sea vector o entero)
 void InsertarLista(nodo_simple*& actual, int vector) {
 	struct nodo_simple* nuevo = new nodo_simple();
+	/*
+	for(int i=0; i<4;i++){
+		nuevo->dato[i] = vector[i];	
+	}
+	*/
 	nuevo->dato = vector;
 	nuevo->next = nullptr;
 	struct nodo_simple* aux = actual;
@@ -33,6 +39,11 @@ void InsertarLista(nodo_simple*& actual, int vector) {
 //Muestra los elementos que contiene la lista
 void MostrarSimple(nodo_simple* lista) {
 	while (lista) {  //mientras aux!=null
+		/*
+		for(int i=0; i<4;i++){
+			cout << lista->dato[i] << endl;
+		}
+		*/
 		cout << lista->dato << endl;
 		lista = lista->next;
 	}
