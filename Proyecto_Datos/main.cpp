21fd0684047cf97d9152a52c6598d9cde70facce
joan899 <51293191+joan899@users.noreply.github.com>
2019-09-08 11:31:21 -0600

    Create main.cpp

---
+++ Proyecto_Datos/main.cpp
@@ -0,0 +1,29 @@
+// C++ program to compute factorial of big numbers 
+#include<iostream> 
+#include<string.h>
+#include<string>
+#include "Lista.h"
+using namespace std;
+
+// Driver program 
+int main() {
+	
+	// Take any string 
+	string s1 = "4894351688451728769255875819203593712573192578591813957937159217863918276973967263971836279632878927";
+	int *vector = new int[s1.size()];
+
+	for (int i = 0; i < s1.size();i++) {
+		vector[i] = stoi(s1.substr(i, 1));
+	}
+
+	struct nodo_simple* inicio = nullptr;
+
+	for (int i = 0; i < s1.size();i++) {
+		InsertarLista(inicio,vector[i]);
+	}
+	
+	MostrarSimple(inicio);
+	
+	cout << endl;
+	return 0;
+}
