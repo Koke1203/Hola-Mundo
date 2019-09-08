1748265608374a53b97f2b262b6e1ca3f99f4d1f
JOAN <joan.corea.aguilar@est.una.ac.cr>
2019-09-08 12:48:39 -0600

    main

--- Proyecto_Datos/Main.cpp
+++ Proyecto_Datos/Main.cpp
@@ -8,8 +8,7 @@ using namespace std;
 // Driver program 
 int main() {
 
-	// Take any string 
-	string s1 = "4894351688451728769255875819203593712573192578591813957937159217863918276973967263971836279632878927";
+	
 
 
 	cout << endl;
