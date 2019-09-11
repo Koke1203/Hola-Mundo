00330d8d38652806c41ba303126fe5fb2e24039c
Jorge Mauricio Canales <jorge.jordi@hotmail.com>
2019-09-10 18:25:31 -0600

    Commit a las operaciones basicas

--- Proyecto_Datos/operaciones_basicas.cpp
+++ Proyecto_Datos/operaciones_basicas.cpp
@@ -9,24 +9,24 @@ using namespace std;
 Integer* SumaDigito(Integer* num1, Integer* num2) {
 	Integer* suma = NULL;
 	//aqui tengo que leer la lista, ver si se ocupa un for o algo asi, igual en las demas operaciones.
-	suma = num1+num2;
+	//suma = num1+num2;
 	return suma;
 }
 
 Integer* RestaDigito(Integer* num1, Integer* num2) {
 	Integer* resta = NULL;
-	resta = num1 + num2;
+	//resta = num1 + num2;
 	return resta;
 }
 
 Integer* MultiplicacionDigito(Integer* num1, Integer* num2) {
 	Integer* multiplicacion = NULL;
-	multiplicacion = num1 * num2;
+	//multiplicacion = num1 * num2;
 	return multiplicacion;
 }
 
 Integer* DivisionDigito(Integer* num1, Integer* num2) {
 	Integer* division = NULL;
-	division = num1 / num2;
+	//division = num1 / num2;
 	return division;
 }
