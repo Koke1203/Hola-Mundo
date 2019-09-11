e51bf7c0c018afce84d65a6741aceb4a231a28a5
Jorge Mauricio Canales <jorge.jordi@hotmail.com>
2019-09-10 18:01:14 -0600

    Commit to math basics methods

--- Proyecto_Datos/operaciones_basicas.cpp
+++ Proyecto_Datos/operaciones_basicas.cpp
@@ -8,24 +8,24 @@ using namespace std;
 
 Integer* SumaDigito(Integer* num1, Integer* num2) {
 	Integer* suma = NULL;
-	suma = num1+num2;
+	//suma = num1+num2;
 	return suma;
 }
 
 Integer* RestaDigito(Integer* num1, Integer* num2) {
 	Integer* resta = NULL;
-	resta = num1 + num2;
+	//resta = num1 + num2;
 	return resta;
 }
 
 Integer* MultiplicacionDigito(Integer* num1, Integer* num2) {
 	Integer* multiplicacion = NULL;
-	multiplicacion = num1 * num2;
+	//multiplicacion = num1 * num2;
 	return multiplicacion;
 }
 
 Integer* DivisionDigito(Integer* num1, Integer* num2) {
 	Integer* division = NULL;
-	division = num1 / num2;
+	//division = num1 / num2;
 	return division;
 }
